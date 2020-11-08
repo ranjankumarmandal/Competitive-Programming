f3b18910cff687e4cefbc4a430e94d9e4e886480
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2020-11-08 23:35:23 +0530

    Update Pattern7.java

--- dsAndAlgo_Foundation_java/patterns/Pattern7.java
+++ dsAndAlgo_Foundation_java/patterns/Pattern7.java
@@ -1,5 +1,28 @@
 package dsAndAlgo_Foundation_java.patterns;
 
-public class Pattern7 {
+import java.util.Scanner;
 
+/*
+*	
+	*	
+		*	
+			*	
+				*	
+ */
+public class Pattern7 {
+	public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int nr = scn.nextInt();
+        // write ur code here
+        
+        for(int cr = 1; cr <= nr; cr++) {
+            for(int cc = 1; cc <= nr; cc++) {
+                if(cr == cc) System.out.print("*\t");
+                else System.out.print("\t");
+            }
+            
+            System.out.println();
+        }
+
+    }
 }
