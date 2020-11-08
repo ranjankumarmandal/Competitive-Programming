59f1b711e60a2ed486680de28fa729e296b1d579
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2020-11-08 23:36:20 +0530

    Update Pattern10.java

--- dsAndAlgo_Foundation_java/patterns/Pattern10.java
+++ dsAndAlgo_Foundation_java/patterns/Pattern10.java
@@ -1,5 +1,41 @@
 package dsAndAlgo_Foundation_java.patterns;
 
-public class Pattern10 {
+import java.util.Scanner;
 
+/*
+		*	
+	*		*	
+*				*	
+	*		*	
+		*	
+ */
+public class Pattern10 {
+	public static void main(String[] args) {
+	    Scanner scn = new Scanner(System.in);
+	    int nr = scn.nextInt();
+	    int nosp = nr / 2;
+	    int nosp1 = 1;
+	     // write ur code here
+	     
+	     for(int cr = 1; cr <= nr; cr++) {
+	         for(int csp = 1; csp <= nosp; csp++) System.out.print("\t");
+	         if(cr == 1 || cr == nr) System.out.print("*");
+	         else {
+	             System.out.print("*");
+	             for(int csp = 1; csp <= nosp1 - 1; csp++) System.out.print("\t");
+	             System.out.print("*");
+	         };
+	         
+	         if(cr <= nr / 2) {
+	             nosp--;
+	             nosp1 = nosp1 + 2;
+	         } else {
+	             nosp++;
+	             nosp1 = nosp1 - 2;
+	         }
+	         
+	         System.out.println();
+	     }
+
+	 }
 }
